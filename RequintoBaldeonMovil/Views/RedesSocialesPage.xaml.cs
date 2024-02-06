3d20e517235db6c38f21e34a931e84b6fbae91bb
aztek <aztek@MG-PAUL-BALDEON>
2024-02-06 17:21:13 -0500

    Agregar archivos de proyecto.

---
+++ RequintoBaldeonMovil/Views/RedesSocialesPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RequintoBaldeonMovil.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RedesSocialesPage : ContentPage
+    {
+        public RedesSocialesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
